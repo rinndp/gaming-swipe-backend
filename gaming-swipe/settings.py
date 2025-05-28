e2cd29566f971dcbcb45b606f76d4664d48e0032
axelPortatil <axelperez221105@gmail.com>
2025-05-28 14:58:12 +0200

    imporving user admin

--- gaming-swipe/settings.py
+++ gaming-swipe/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST", 'localhost'),
-        "PORT": os.getenv("DB_PORT", 3306),
+        "PORT": os.getenv("DB_PORT", 5432),
     }
 }
 
--- users/admin/user_admin.py
+++ users/admin/user_admin.py
@@ -7,7 +7,7 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('email', 'name', 'last_name', 'updated_at', 'is_superuser', 'is_staff', 'is_active')
     list_filter = ("is_superuser", "is_active", "is_staff")
     list_editable = ('is_staff', 'is_superuser', 'is_active',)
-
+    readonly_fields = ('email', 'name', 'last_name', 'slug',)
     search_fields = ("email","name",)
     ordering = ('-updated_at',)
 
