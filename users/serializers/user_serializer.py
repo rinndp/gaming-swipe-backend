960c628799910c137b3bdbc41ae65a9ad295f066
axelPortatil <axelperez221105@gmail.com>
2026-01-13 12:50:31 +0100

    deleting conflicting import in user_serializer

--- users/serializers/user_serializer.py
+++ users/serializers/user_serializer.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-from rest_framework.authtoken.admin import User
-
 from users.models import CustomUser
 
 class SearchUserSerializer(serializers.ModelSerializer):
